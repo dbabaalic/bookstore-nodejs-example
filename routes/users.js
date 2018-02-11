5ee7cec6ccdd9cc15b9086aa023c0fb3eac6e966
Damir Baba-Alic <d.babaalic@levi9.com>
2018-02-11 22:39:16 +0100

    Reading from req.body

--- routes/users.js
+++ routes/users.js
@@ -3,13 +3,7 @@ const router = express.Router();
 const Membership = require('membership')();
 
 router.post('/register', async (req, res, next) => {
-    let newUser = {
-        email: req.body.email,
-        password: req.body.password,
-        confirmPassword: req.body.confirmPassword,
-    };
-    
-    let result = await Membership.registerMember(newUser);
+    let result = await Membership.registerMember(req.body);
     res.json(result);
 });
 
